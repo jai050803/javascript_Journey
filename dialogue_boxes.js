4bf276b6e2687570eebbdd9fab9220497a6cf2ef
jai <sjai5803@gmail.com>
2024-01-01 09:40:13 +0530

    javascript day1

---
+++ dialogue_boxes.js
@@ -0,0 +1,22 @@
+// Javascript Alert, Prompt and the Confirm method
+
+/* This creates a dialogue boxes for the users to request a response from the user and to pause all the code in background until the request is satisfied.
+
+all these are the methods of the windows object. This object is the highest level object that JavaScript can deal with in a browser.*/
+
+windows.alert("hello world");
+
+/*Writing the windows is not necessary. Its presence is assumed even if omitted. */
+
+alert("hello world");
+
+/*the end of line for javascript is semi-colon (;) */
+
+
+//other methods
+
+windows.document.write("hello world");
+document.write("hello world");
+window.alert( message );
+window.confirm( message );
+window.prompt( message, default_response );
---
+++ js_firstCode.js
@@ -0,0 +1,20 @@
+// javascript comments
+
+// this is a single line comment it ends with a new line
+
+/* this is a multiline comment. it can be in more than one line. -
+to end the multiline comment you need to add this */
+
+
+// using the console.log() in javascript
+
+/*This is used to debugging purposes.*/
+
+console.log("hello world");
+
+/*The console log has no explicit return value.*/
+
+
+
+
+
